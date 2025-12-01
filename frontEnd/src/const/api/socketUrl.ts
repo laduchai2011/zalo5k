dd54f67660ef60fadee67155dda770c0657ca37d
La <laduchai20111995@gmail.com>
2025-12-01 13:15:10 +0700

    message 7

--- frontEnd/src/const/api/socketUrl.ts
+++ frontEnd/src/const/api/socketUrl.ts
@@ -1,3 +1,3 @@
 const isProduct = process.env.NODE_ENV === 'production';
 
-export const SOCKET_URL = isProduct ? process.env.API_URL : 'http://zalo5k.local.com:3005';
+export const SOCKET_URL = isProduct ? process.env.SOCKET_URL : 'http://zalo5k.local.com:3005';
